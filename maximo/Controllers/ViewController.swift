5806b8c8f249f1c38ba521f54d3c9dd45fb18074
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-07-12 17:28:27 -0300

    adiciona atividade 12 de julho

--- maximo/Controllers/ViewController.swift
+++ maximo/Controllers/ViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet  var nameTextField: UITextField!
-   @IBOutlet var felicidadeTextField: UITextField!
+   @IBOutlet weak var felicidadeTextField: UITextField!
     
     @IBAction func adicionar(_ sender: Any) {
-        let nome  = nameTextField.text ?? ""
-        let felicidade  = Int(felicidadeTextField.text ?? "0") ?? 0
+  
+        let nome = nameTextField.text
+        let felicidade : Int! = Int(felicidadeTextField.text!)
         
-        let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+        let refeicao = Refeicao(nome: nome!, felicidade: felicidade)
         
-        print("comi \(nome) e fiquei com felicidade: \(felicidade)")
+        
+        
+        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.nome)")
      }
     
    }
