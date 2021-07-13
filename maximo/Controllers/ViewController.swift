fc708837fb1940387ba1b315e6ac7bb7632976ce
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-07-13 19:42:59 -0300

    adiciona atividade 13 de julho corrigida

--- maximo/Controllers/ViewController.swift
+++ maximo/Controllers/ViewController.swift
@@ -9,28 +9,20 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
-    @IBOutlet  var nameTextField: UITextField!
-   @IBOutlet weak var felicidadeTextField: UITextField!
+    
+    @IBOutlet  var nameTextField: UITextField?
+    @IBOutlet weak var felicidadeTextField: UITextField?
     
     @IBAction func adicionar(_ sender: Any) {
-  
-        let nome = nameTextField.text
-        let felicidade : Int! = Int(felicidadeTextField.text!)
-        
-        let refeicao = Refeicao(nome: nome!, felicidade: felicidade)
-        
-        
-        
-        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.nome)")
-     }
-    
-   }
-
-
-
-
-
-
-
-
+        if let nomeDaRefeicao = nameTextField?.text, let felicidadeDaRefeicao =
+            felicidadeTextField?.text {
+            let nome = nomeDaRefeicao
+            if let felicidade = Int (felicidadeDaRefeicao) {
+                let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+                print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.nome)")
+            } else {
+                print("erro ao tentar criar a refeicao")
+            }
+        }
+    }
+}
