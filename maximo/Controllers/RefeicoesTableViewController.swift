117893569d539d54b6bd9c935964fa768afc6fa1
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-07-27 17:36:21 -0300

    adiciona atividade 27 de julho parte 2/3

--- maximo/Controllers/RefeicoesTableViewController.swift
+++ maximo/Controllers/RefeicoesTableViewController.swift
@@ -17,12 +17,33 @@ class RefeicoesTableViewController: UITableViewController, AdicionarRefeicaoDele
         let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
         let refeicao = refeicoes[indexPath.row]
         celula.textLabel?.text = refeicao.nome
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(mostrarDetalhes(_:)))
+        celula.addGestureRecognizer(longPress)
+        
         return celula
     }
     func add(_ refeicao: Refeicao){
         refeicoes.append(refeicao)
         tableView.reloadData()
 }
+    
+    @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let celula = gesture.view as! UITableViewCell
+            
+            guard let indexPath = tableView.indexPath(for: celula) else { return }
+            
+            let refeicao = refeicoes[indexPath.row]
+            
+            let alerta = UIAlertController(title: refeicao.nome, message: "felicidade:\(refeicao.felicidade)", preferredStyle: .alert)
+
+            
+            
+            present(alerta, animated: true, completion: nil)
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "Adicionar" {
